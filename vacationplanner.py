f5e1e1da4b5cceaadd8c777a8012ea55000fb3a1
Filip Tosic <tosicfilip3@gmail.com>
2024-02-01 19:21:44 -0700

    simplified apply_for_week function and removed need for weeks variable

--- vacationplanner.py
+++ vacationplanner.py
@@ -4,10 +4,6 @@ print(this_year)
 
 #create weeks of year dict
 
-weeks_of_year = {}
-for i in range(1, 53):
-    weeks_of_year[i] = 0
-
 who_is_off = {}
 for i in range(1, 53):
     who_is_off[i] = []
@@ -52,48 +48,43 @@ class Employee:
 
     def get_desired_weeks(self):
         self.desired_weeks = []
-        weeks_selected = 1
-        while weeks_selected < self.weeks_of_entitlement + 1:
+        weeks_selected = 0
+        while weeks_selected < self.weeks_of_entitlement:
             options = []
-            attempts = 1
-            while attempts < 4:
+            attempts = 0
+            while attempts < 3:
                 try:
-                    desired = int(input(f"Enter vacation week {weeks_selected} option {attempts} : "))
+                    desired = int(input(f"Enter vacation week {weeks_selected + 1} option {attempts + 1} : "))
                     options.append(desired)
                 except ValueError:
                     print("Please enter INT's only")
                 attempts += 1
             self.desired_weeks.append(options)
             weeks_selected += 1
-
-def apply_for_week(employee, current_entitlement):
-    for week in employee.desired_weeks[current_entitlement]:
-        for key in week:
-            if weeks_of_year[week[key]] < 2:
-                weeks_of_year[week[key]] += 1
-                who_is_off[week[key]].append(employee.name)
-                return True
-
+     
+def apply_for_week(employee):
+    
+    for week in employee.desired_weeks[0]:
+        print(week)
+        if len(who_is_off[week]) < 2:
+            who_is_off[week].append(employee.name)
+            return True
 
 emp1 = Employee("Jim", 2021, 175925)
 emp2 = Employee("Pam", 2022, 918348)
 emp3= Employee("Dwight", 2022, 192080)
 emp4 = Employee("Michael", 2004, 49019)
-# print(emp1.weeks_of_entitlement)
-# emp1.get_desired_weeks()
-# # print(emp1.desired_weeks)
-# # emp2.get_desired_weeks()
-# # emp3.get_desired_weeks()
-# # # # print(Employee.emp_list)
-# # apply_for_week(emp1)
-# # apply_for_week(emp2)
-# # apply_for_week(emp3)
-# print(emp1.desired_weeks)
-# print(who_is_off, weeks_of_year)
-# Employee.emp_list.sort(key=Employee.sort_by_seniority)
-# sorted_emps = Employee.emp_list
-# print(sorted_emps)
-print(Employee.emp_list)
+
+emp1.get_desired_weeks()
+
+emp2.get_desired_weeks()
+emp3.get_desired_weeks()
+
+apply_for_week(emp1)
+apply_for_week(emp2)
+apply_for_week(emp3)
+print(who_is_off)
+
 
 
 
